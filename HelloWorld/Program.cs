fd195fe59b62a56a4d64972aac7f19702159be1d
Meryem <jerrai.meryem.mp@gmail.com>
2025-09-30 17:36:39 +0200

    Ajoutez des fichiers projet.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            var stopsign = Console.ReadLine(); 
+            Console.WriteLine($"{Environment.NewLine}What's next after {stopsign}");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d}, at {currentDate:t}");
+            Console.WriteLine($"{Environment.NewLine}Press any key to exit darling :)");
+            
+
+
+
+        }
+    }
+}
